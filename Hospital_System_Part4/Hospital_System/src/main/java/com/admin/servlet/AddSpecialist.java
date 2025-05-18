1da7ff83f36ca01af83b72cf08708a3850f286ec
hasaranga15 <hasarangachamod14@gmail.com>
2025-05-18 18:48:05 +0530

    Add files via upload

---
+++ Hospital_System_Part4/Hospital_System/src/main/java/com/admin/servlet/AddSpecialist.java
@@ -0,0 +1,34 @@
+package com.admin.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/addSpecialist")
+public class AddSpecialist extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		String specName=req.getParameter("specName");
+	     
+	     SpecialistDao dao=new SpecialistDao(DBConnect.getConn());
+	     boolean f=dao.addSpecialist(specName);
+	     
+	     HttpSession session = req.getSession();
+	     
+	     if(f) {
+	    	 session.setAttribute("succMsg","Specialist Added");
+	    	 resp.sendRedirect("admin/index.jsp");
+	     } else {
+	    	 session.setAttribute("errorMsg", "something wrong on server");
+	    	 resp.sendRedirect("admin/index.jsp");
+	     }
+		
+	}
+	
+
+}
